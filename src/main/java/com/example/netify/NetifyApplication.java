f230c42072becf0d48d022ea2a6a4e8c7d3d784b
admin <korbut2702@gmail.com>
2023-11-03 14:13:12 +0300

    Initial commit.

---
+++ src/main/java/com/example/netify/NetifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.netify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/netify/NetifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
